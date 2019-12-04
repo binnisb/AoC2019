1c12435cc3ace4180fe69735c01d53dbb8faf893
Brynjar Smári Bjarnason <binni@binnisb.com>
2019-12-04 23:03:48 +0100

    functional solution to 4

--- tests/test_des4.py
+++ tests/test_des4.py
@@ -3,10 +3,12 @@ import pytest
 
 @pytest.mark.parametrize("test,result", [("111111",True),("223450",False),("123789",False)])
 def test_is_password(test,result):
-    assert des4.Passwords.is_password(test) == result
+    assert des4.is_password(test) == result
 
 @pytest.mark.parametrize("test,result", [(("111111", "111122"),10),(("111222", "111240"),15)])
-def test_count(test,result):
-    pw = des4.Passwords(*test)
-    assert pw.count_passwords() == result
+def test_count_large_group(test,result):
+    assert des4.count_pw(*test, True) == result
 
+@pytest.mark.parametrize("test,result", [(("111111", "111122"),1),(("111222", "111240"),8)])
+def test_count_must_be_2(test,result):
+    assert des4.count_pw(*test, False) == result
